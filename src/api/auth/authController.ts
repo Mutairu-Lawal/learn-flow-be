62e94c38d17afa73d398d8c84989f89297b8e938
Mutairu <thebillionnairadream@gmail.com>
2026-05-31 23:38:53 +0100

    implement user creation logic in AuthService and update signUp method in AuthController

--- src/api/auth/authController.ts
+++ src/api/auth/authController.ts
@@ -1,12 +1,15 @@
 import type { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { ServiceResponse } from "@/common/models/serviceResponse";
+import { authService } from "./authService";
 
 class AuthController {
 	signUp = async (req: Request, res: Response) => {
 		// In a real implementation, you would handle user creation logic here
-		const { username, email } = req.body;
-		const serviceResponse = ServiceResponse.success("message", { username, email }, StatusCodes.OK);
+		const { username, email, password } = req.body;
+
+		const serviceResponse = await authService.createUser(username, email, password);
+
 		res.status(serviceResponse.statusCode).send(serviceResponse);
 	};
 
--- src/api/auth/authService.ts
+++ src/api/auth/authService.ts
@@ -1,5 +1,40 @@
+import { StatusCodes } from "http-status-codes/build/cjs/status-codes";
+import { ServiceResponse } from "@/common/models/serviceResponse";
+import { hashPassword } from "@/common/utils/bcrypt";
+
 export class AuthService {
 	// Dependency injection of the AuthRepository
+	// check if user already exists, hashpassword, save to database. Return a ServiceResponse with the created user or an error message.
+	createUser = async (username: string, email: string, password: string) => {
+		try {
+			const hashedPassword = await hashPassword(password);
+
+			// const user = await this.authRepository.create({
+			// 	username,
+			// 	email,
+			// 	password_hash: hashedPassword,
+			// });
+
+			return ServiceResponse.success(
+				"User created successfully",
+				{ username, email, hashedPassword },
+				StatusCodes.CREATED,
+			);
+		} catch (error) {
+			return ServiceResponse.failure(
+				"User creation failed",
+				{
+					error: error instanceof Error ? error.message : "Unknown error",
+				},
+				StatusCodes.INTERNAL_SERVER_ERROR,
+			);
+		}
+	};
+
+	// authenticateUser = async (username: string, password: string) => {
+	// 	// In a real implementation, you would handle authentication logic here
+	// 	return { username };
+	// };
 }
 
 export const authService = new AuthService();
